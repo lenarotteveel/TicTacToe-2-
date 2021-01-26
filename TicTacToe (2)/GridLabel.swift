5c1ee532c080bf2511f2b9a01175bc909ae0d0ae
Lena Rotteveel <lenarotteveel@gmail.com>
2021-01-26 11:09:48 -0600

    Final Project

---
+++ TicTacToe (2)/GridLabel.swift
@@ -0,0 +1,15 @@
+//
+//  GridLabel.swift
+//  TicTacToe (2)
+//
+//  Created by Lena Rotteveel on 11/20/19.
+//  Copyright © 2019 Lena Rotteveel. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel {
+
+    var canTap = true 
+
+}
